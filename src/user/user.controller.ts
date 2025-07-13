ed397b09c95ad253a5c67b6fb8582b286178ff02
hungdangquang149 <dangqhung149@gmail.com>
2025-07-13 23:44:57 +0700

    get stream file

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException, UploadedFiles } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException, UploadedFiles, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -7,19 +7,69 @@ import { LoginUserDto } from './dto/login-user.dto';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { storage } from './oss';
 import * as path from 'path';
+import * as fs from 'fs'
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) { }
 
 
+  // merge file
+  @Get('merge/file')
+  mergeFile(@Query("file") fileName: string) {
+    const nameDir = 'uploads/' + fileName
+
+    //read
+    const files = fs.readdirSync(nameDir)
+
+    let startPos = 0, countFile = 0
+    files.map(file => {
+      //get path full
+      const filePath = nameDir + '/' + file
+      console.log("filePath | ", filePath)
+
+      const streamFile = fs.createReadStream(filePath)
+      streamFile.pipe(fs.createWriteStream('uploads/merge/' + fileName, {
+        start: startPos
+      })).on('finish', () => {
+        countFile++
+        console.log('countFile |', countFile)
+        if (files.length === countFile) {
+
+          fs.rm(nameDir, {
+            recursive: true
+          }, () => { })
+        }
+      })
+
+      startPos += fs.statSync(filePath).size
+    })
+  }
+
+
   @Post('upload/large-file')
   @UseInterceptors(FilesInterceptor('files', 20, {
     dest: 'uploads',
   }))
-  uploadLargeFile(@UploadedFiles() files: Array<Express.Multer.File>, @Body() body) {
+  uploadLargeFile(@UploadedFiles() files: Array<Express.Multer.File>, @Body() body: { name: string }) {
     console.log('upload file body |', body)
     console.log('upload files |', files)
+    // get file
+
+    const fileName = body.name.match(/(.+)-\d+$/)?.[1] ?? body.name
+    const nameDir = 'uploads/chunks-' + fileName
+
+    // mkdir
+    if (!fs.existsSync(nameDir)) {
+      fs.mkdirSync(nameDir)
+    }
+
+    // parse file
+    fs.cpSync(files[0].path, nameDir + '/' + body.name)
+
+    // remove redundant
+    fs.rmSync(files[0].path)
+
   }
 
 
